c42a34782a64778423283c0fe0fe650e8179cf90
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2022-11-04 21:57:46 +0300

    feat add images in artikles

---
+++ scrollToTop.js
@@ -0,0 +1,15 @@
+const scrollToTop = () => {
+  const topBtn = document.querySelector("#scrollToTopButton");
+
+  topBtn.addEventListener("click", (event) => {
+    console.log(event);
+    event.preventDefault();
+    seamless.scrollIntoView(document.querySelector("#header"), {
+      behavior: "smooth",
+      block: "center",
+      inline: "center",
+    });
+  });
+};
+
+scrollToTop();
