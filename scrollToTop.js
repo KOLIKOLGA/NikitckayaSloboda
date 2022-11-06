9a8e18868eb7aa0a30596e1cd1ddcd7a2097097c
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2022-11-06 18:42:53 +0300

    feat: add на верх

--- scrollToTop.js
+++ scrollToTop.js
@@ -1,9 +1,21 @@
 const scrollToTop = () => {
-  const topBtn = document.querySelector("#scrollToTopButton");
+  const toTop = document.querySelector(".button-up");
 
-  topBtn.addEventListener("click", (event) => {
-    console.log(event);
-    event.preventDefault();
+  window.onscroll = () => {
+    // const hasScrolling = !(window.scrollX === 0 && window.scrollY === 0);
+    // let scroll = {
+    //   hasScrolling: hasScrolling,
+    //   scrollY: Math.round(scrollY),
+    // };
+
+    if (scrollY >= 800) {
+      toTop.classList.add("_active");
+    } else {
+      toTop.classList.remove("_active");
+    }
+  };
+
+  toTop.addEventListener("click", () => {
     seamless.scrollIntoView(document.querySelector("#header"), {
       behavior: "smooth",
       block: "center",
